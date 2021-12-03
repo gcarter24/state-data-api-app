3971b12bda8e01d0e488c76f5a9d2c7c6df55eeb
George <gchriscarter@gmail.com>
2021-12-03 08:05:19 -0800

    add some testing logic to see how toaccess test within an app

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -1,3 +1,5 @@
+# require "rspec"
+
 class Api::StatesController < ApplicationController
   def index
     @states = State.all
@@ -8,4 +10,15 @@ class Api::StatesController < ApplicationController
     @state = State.joins(:abbreviations).find_by(abbreviations: { code: params[:code] })
     render "show.json.jb"
   end
+end
+
+# Rspec.describe Api::StatesController do
+#   describe "#index" do
+#     it "should return all states" do
+#       states = State.all 
+#       result = states.index(states)
+#       expect(result).to eq(states)
+#     end 
+#   end 
+# end 
 end
